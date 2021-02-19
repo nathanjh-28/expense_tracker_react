92e692aa5548ce14a90d56b9229429f4f523f0a5
nathanjh-28 <nathan@nathanjharris.com>
2021-02-19 12:33:10 -0700

    add and delete functionality complete

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
 
 export const AddTransaction = () => {
+    const { addTransaction } = useContext(GlobalContext);
     const [text, setText] = useState('')
     const [amount, setAmount] = useState(0)
 
+    const onSubmit = e => {
+        e.preventDefault()
+        const newTransaction = {
+            id: Math.floor(Math.random() * 1000000000),
+            text,
+            amount: +amount
+        }
+        addTransaction(newTransaction);
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
