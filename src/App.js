3b0772577bec87fd1a8d1a8cce8df8d4a40a26eb
nathanjh-28 <nathan@nathanjharris.com>
2021-02-19 09:18:41 -0700

    transaction component and context api working

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionList } from './components/TransactionList'
 import { AddTransaction } from './components/AddTransaction'
 
+import { GlobalProvider } from './context/GlobalState'
+
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -17,7 +19,7 @@ function App() {
         <AddTransaction />
       </div>
 
-    </div>
+    </GlobalProvider>
   );
 }
 
